f5107cdeb14abd2f2617b7dabe1f9b5ec827537d
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-25 16:14:01 +0100

    Es 2

---
+++ js/snack2.js
@@ -0,0 +1,21 @@
+//Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+
+
+const button = document.querySelector("button");
+const result = document.getElementById("output");
+
+button.addEventListener("click", function () {
+    const numberInput = document.getElementById("numbers");
+    let number = parseInt(numberInput.value);
+    if(number%2==0){
+        console.log(number);
+        result.innerHTML = number;
+    } else{
+        number++;
+        console.log(number)
+        result.innerHTML = number;
+    }
+
+
+});
