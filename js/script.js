d5f2079dcf135afc5a39c82280d006406c0517e0
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-25 15:34:47 +0100

    Insert numbers in array

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+Crea un array vuoto.
+Chiedi per 6 volte all'utente di inserire un numero,
+se è dispari inseriscilo nell'array.
+*/
+
+
+const numbers = [];
+
+for (let i = 0; i < 6; i++) {
+    const inputNumber = parseInt(prompt("Inserisci un numero: "));
+
+    if (inputNumber % 2 != 0){
+        numbers[i] = inputNumber;
+        console.log(numbers[i]);
+    } else{
+        console.log("il numero è pari");
+    }
+
+        
+}
