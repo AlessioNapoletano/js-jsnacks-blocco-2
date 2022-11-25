cb61960654a53ebeaa638d3dc0e36172963dc133
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-25 15:44:29 +0100

    Modified index

--- js/script.js
+++ js/script.js
@@ -7,15 +7,16 @@ se è dispari inseriscilo nell'array.
 
 const numbers = [];
 
+const UlElement = document.getElementById("list-numbers");
+
 for (let i = 0; i < 6; i++) {
     const inputNumber = parseInt(prompt("Inserisci un numero: "));
 
     if (inputNumber % 2 != 0){
         numbers[i] = inputNumber;
+        const newLiElement = document.createElement("li");
+        newLiElement.innerHTML += "il numero inserito nell'array è: " + numbers[i];
+        UlElement.append(newLiElement);
     } 
-
-    
-
-        
 }
 console.log(numbers)
