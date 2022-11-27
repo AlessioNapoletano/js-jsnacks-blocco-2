94489d607c51830fb4c896dce5bc94bbb4c263d7
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-27 12:43:41 +0100

    Modied script es3

--- 3 - Esercizio/js/script.js
+++ 3 - Esercizio/js/script.js
@@ -1,30 +1,41 @@
-//Calcola la somma dei primi 10 numeri di un array.
+/*
+Calcola la somma dei primi 10 numeri di un array.
+Calcola la somma e la media dei primi 10 numeri di un array.
+Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
+*/
 
-const number = 
-[
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12
+const number =
+    [
+        1,
+        2,
+        3,
+        44,
+        5,
+        6,
+        78,
+        8,
+        9,
+        10,
+        11,
+        12
 
-];
+    ];
+
 let sum = 0;
+let average = 0;
+let max = 0;
 
 
-for(let i=0; i<10; i++){
-    sum+=number[i];
+for (let i = 0; i < 10; i++) {
+    sum += number[i];
+
+    if (number[i] > max) {
+        max = number[i];
+    }
 }
 
-let average = sum/10;
+average = sum / 10;
 
-
-console.log(sum);
-console.log(average);
+console.log("Il numero massimo inserito all'interno dell'array è: " + max);
+console.log("La somma dei primi dieci numeri è: " + sum);
+console.log("La media dei dieci numeri è : " + average);
