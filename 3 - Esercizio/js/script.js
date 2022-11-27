8b4b737c60c29eac194599524c15c425053b7288
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-27 12:54:18 +0100

    Output in index

--- 3 - Esercizio/js/script.js
+++ 3 - Esercizio/js/script.js
@@ -3,6 +3,9 @@ Calcola la somma dei primi 10 numeri di un array.
 Calcola la somma e la media dei primi 10 numeri di un array.
 Calcola la somma, la media e il valore massimo dei primi 10 numeri di un array.
 */
+const sumOutput = document.getElementById("sum");
+const averageOutput = document.getElementById("average");
+const maxOutput = document.getElementById("max");
 
 const number =
     [
@@ -39,3 +42,7 @@ average = sum / 10;
 console.log("Il numero massimo inserito all'interno dell'array è: " + max);
 console.log("La somma dei primi dieci numeri è: " + sum);
 console.log("La media dei dieci numeri è : " + average);
+
+sumOutput.innerHTML = "La somma dei primi 10 numeri è: " + sum;
+averageOutput.innerHTML = "La media dei primi 10 numeri è: " + average;
+maxOutput.innerHTML = "Il numero massimo inserito è: " + max;
