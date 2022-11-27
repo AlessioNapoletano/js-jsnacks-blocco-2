501738d5cc018afd2f11472e5e4ea20023cdf529
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-27 12:34:21 +0100

    changed folders and renamed

--- 3 - Esercizio/js/script.js
+++ 3 - Esercizio/js/script.js
@@ -11,12 +11,20 @@ const number =
     7,
     8,
     9,
-    10
+    10,
+    11,
+    12
+
 ];
 let sum = 0;
+
 
 for(let i=0; i<10; i++){
     sum+=number[i];
 }
 
+let average = sum/10;
+
+
 console.log(sum);
+console.log(average);
