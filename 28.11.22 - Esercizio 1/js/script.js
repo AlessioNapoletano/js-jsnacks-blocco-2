8b6b915c0c402eeac04c8e0e09e690d1b3f5cff6
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-28 10:35:34 +0100

    with do-while

--- 28.11.22 - Esercizio 1/js/script.js
+++ 28.11.22 - Esercizio 1/js/script.js
@@ -3,9 +3,18 @@
 
 const number = 2;
 let result = 0;
+let i = 0;
 
-
-for (let i = 0; i <= 1000; i++) {
-    result = number * i;
+do {
+    result = number ** i;
+    i++;
     console.log(result);
-}
+} while (result <= 1000);
+
+
+
+/*
+for (let i = 0; i <= 1000; i++) {
+    result = number ** i;
+    console.log(result);
+}*/
