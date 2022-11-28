010dca44e6107ebed945e6c30b9d8b7173bb0780
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-28 10:49:38 +0100

    Esercizio 2

---
+++ 28.11.22 - Esercizio 2/js/script.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Generatore di persone fittizie casuali:
+ * Il Grande Gatsby ha una lista di nomi e una lista di cognomi, da queste vuole generare una falsa lista di invitati con nome e cognome.
+ *
+ */
+
+const names = ['Michela', 'Giuseppina', 'Taylor', 'Donna', 'Marsha', 'Fabio', 'Roberto'];
+const lastnames = ['Cappelletti', 'Jager', 'Porcinelli', 'Buffetti', 'Forghieri', 'Papagni', 'Marazzini'];
+
+
+let person = [];
+
+for (let i = 0; i < names.length; i++) {
+    person = names[i] + " " + lastnames[i];
+    console.log(person);
+}
