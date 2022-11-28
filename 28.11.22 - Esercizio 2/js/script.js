6d1a5aeb29b0c5bd3902f23b7abcda3d7190b331
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-28 10:56:33 +0100

    Output in index

--- 28.11.22 - Esercizio 2/js/script.js
+++ 28.11.22 - Esercizio 2/js/script.js
@@ -8,12 +8,17 @@
 const names = ['Michela', 'Giuseppina', 'Taylor', 'Donna', 'Marsha', 'Fabio', 'Roberto'];
 const lastnames = ['Cappelletti', 'Jager', 'Porcinelli', 'Buffetti', 'Forghieri', 'Papagni', 'Marazzini'];
 
+const ulElement = document.getElementById("output");
+
 
 let person = [];
 
 for (let i = 0; i < names.length; i++) {
     person[i] = names[i] + " " + lastnames[i];
     console.log(person[i]);
+    const newLiElement = document.createElement("li");
+    newLiElement.innerHTML = person[i];
+    ulElement.append(newLiElement);
 }
 
 console.log(person);
