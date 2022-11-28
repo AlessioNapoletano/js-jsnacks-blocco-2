533e54e676bbfb4146640cab4473623846df4b12
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-28 12:44:12 +0100

    Add Manipolazione Doma

--- 28.11.22 - Esercizio 3/js/script.js
+++ 28.11.22 - Esercizio 3/js/script.js
@@ -1,11 +1,15 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
 const numbers = [10, 22, 33, 44, 55, 60, 70, 80, 90];
-
+const ulElement = document.querySelector("ul.list-group");
 let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
     if (i % 2 != 0) {
+        const newLiElement = document.createElement("li");
+        newLiElement.classList.add("list-group-item");
+        newLiElement.innerHTML = "Numero in posizione dispari: " + numbers[i];
+        ulElement.append(newLiElement);
         console.log("Questo numero è in una posizione dispari, verrà sommatto: " + numbers[i]);    
         sum = sum + numbers[i];
     }
